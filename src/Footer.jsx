05638239aa0fd84ca087229ef8870bca0e9d6c4d
soloKomrade <148073412+gigokomrade@users.noreply.github.com>
2024-01-03 01:47:30 +0000

    latest commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import './Footer.css'; // You can create a separate CSS file for the footer styles if needed
+import React from "react";
+
+import "./Footer.css"; // You can create a separate CSS file for the footer styles if needed
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p className='footer-p'>DATASWIFT NETWORK was built by Brian Itira @2023</p>
+      <p className="footer-p">
+        DATASWIFT NETWORK was built by Brian Itira @2023
+      </p>
     </div>
   );
-}
+};
 
 export default Footer;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import './Home.css';
-import Footer from './Footer';
+import React from "react";
 
-import { Link } from 'react-router-dom/dist';
+import "./Home.css";
+import Footer from "./Footer";
+
+import { Link } from "react-router-dom/dist";
 
 const Home = () => {
   return (
@@ -16,10 +17,17 @@ const Home = () => {
         <section className="featured-offer">
           <h2>Featured Offer</h2>
           <div className="offer-details">
-          <img src="https://i.ibb.co/MnXXFbz/generate-a-cool-3d-rainbow-colours-splash-text-s-1.jpg" alt="generate-a-cool-3d-rainbow-colours-splash-text-s-1" border="0"/>
+            <img
+              src="https://i.ibb.co/MnXXFbz/generate-a-cool-3d-rainbow-colours-splash-text-s-1.jpg"
+              alt="generate-a-cool-3d-rainbow-colours-splash-text-s-1"
+              border="0"
+            />
             <div>
               <h3>Special Data Package</h3>
-              <p>Get exclusive access to our limited-time data package. Stay connected without worrying about data limits.</p>
+              <p>
+                Get exclusive access to our limited-time data package. Stay
+                connected without worrying about data limits.
+              </p>
             </div>
           </div>
         </section>
@@ -30,19 +38,16 @@ const Home = () => {
             <li>High-speed mobile data</li>
             <li>Flexible and affordable plans</li>
             <li>24/7 customer support</li>
-           
           </ul>
         </section>
       </main>
 
       <footer>
-       
-
         <Link to="/Dialer">
-        <button className="connect-button">Connect to DataSwift</button>
+          <button className="connect-button">Connect to DataSwift</button>
         </Link>
       </footer>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
